4f503e724663d2aa960ca581c53cef65f9c45b82
Jordaniel <akinpeludaniel@gmail.com>
2024-08-30 18:50:12 +0100

    Random Anime Picker Complete

--- app/redux/provider.tsx
+++ app/redux/provider.tsx
@@ -1,8 +1,9 @@
 'use client'
 import { Provider } from'react-redux'
 import { store }  from './store'
+import { ReactNode } from 'react'
 
-export function Providers({ children }){
+export function Providers({ children }:any){
     return(
         <Provider store={store}>
             {children}
